4b84234cfef08a151887fd008d067b831ba48a5f
Nicolas Alonso <nicooalonsoo@gmail.com>
2023-07-06 22:20:40 -0300

    javascript

--- js/index.js
+++ js/index.js
@@ -1,178 +1,261 @@
-console.log("Bienvebidos a ...");
-console.log("Vamos a ver, entonces aca empezamos con la idea de la agenda");
-console.log("El primer paso va a ser darle la bienvenida a quien nos este visitando");
-
-let nombreCompleto= prompt("Ingresa tu nombre completo");
-alert("Te damos la bienvenida "+nombreCompleto);
-
-let edad = parseInt(prompt("Ingresa tu edad"));
-alert("Tienes "+edad+" años");
-
-let cud = prompt ("Cuentan con Certificado Unico de Discapacidad?");
-let tieneObraSocial = prompt ("Cuenta con Obra Social?");
-
-if((cud == ("SI") || cud == ("si") || cud == ("Si")) && (tieneObraSocial == ("si") || tieneObraSocial == ("SI") || tieneObraSocial == ("Si"))){
-    alert("Perfecto, podes contar con nuestros servicios");
-}else{
-    alert("Disculpe, necesitamos que cuente con ambos requisitos");
+function guardarDatos() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    localStorage.setItem("nombre", nombre);
+    localStorage.setItem("apellido", apellido);
+    alert("Datos guardados correctamente.");
 }
 
-let obraSocial = prompt("Vamos a comenzar con tu busqueda, ¿nos decis si contas con alguna de estas Obras Sociales, por favor?\n1-OSECAC\n2-OSPECON\n3-UNION PERSONAL\n4-OSDEPYM\n5-OSCHOCA");
+function actualizarHorarios() {
+    let turno = document.getElementById("turno").value;
+    let horariosManana = document.getElementById("horariosManana");
+    let horariosTarde = document.getElementById("horariosTarde");
 
+    if (turno === "manana") {
+        horariosManana.style.display = "block";
+        horariosTarde.style.display = "none";
+    } else if (turno === "tarde") {
+        horariosManana.style.display = "none";
+        horariosTarde.style.display = "block";
+    }
+}
 
-if (obraSocial == "1"){
-    alert("Obra Social de los empleados del comercio, si la trabajmos, podemos proseguir");
-}else if(obraSocial=="2") {
-    alert("Obra Social de la construccion, si la trabajmos, podemos proseguir");
-}
-else if(obraSocial=="3") {
-    alert("Obra Social, del personal de la nacion, si la trabajmos, podemos proseguir");
-}
-else if(obraSocial=="4") {
-    alert("Obra Social de los monotributistas, si la trabajmos, podemos proseguir");
-}
-else if(obraSocial=="5") {
-    alert("Obra Social de los choferes y camioneros, si la trabajmos, podemos proseguir");
-}
-else{
-    alert("Disculpe, esa obra social no la trabajamos al parecer, nos puede enviar a nuestro mail el instructivo de facturacion, por favor");
-}
-let otraObraSocial = prompt("Ingresa el nombre de tu obra social (s-salir)");
+function solicitarTurno() {
+    let edad = document.getElementById("edad").value;
+    let obraSocial = document.getElementById("obraSocial").value;
+    let turno = document.getElementById("turno").value;
+    let horario = document.getElementById("horario").value;
+    let especialidad = document.getElementById("especialidad").value;
 
-function tipoDeObraSocial(otraObraSocial){
+    const resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = "Solicitar turno:<br><br>Edad: " + edad + "<br>Obra Social: " + obraSocial + "<br>Turno: " + turno + "<br>Horario: " + horario + "<br>Especialidad: " + especialidad;
+}
 
-while(otraObraSocial != "s"){
-    //uso el switch para evaluar si esta es otra OS de las que no esta en la lista principal
-    switch(otraObraSocial){
-        case "ospip":
-            alert("ospip, del plastico, si la trabajamos");
-            break;
-        case "elevar":
-            alert("elevar, de pasteleros, si la trabajamos");
-            break;
-        case "ospacp":
-            alert("ospacp, de casas particulares, si la trabajamos");
-            break;
-        case "ospaca":
-            alert("ospaca, del personal del ACA, si la trabajamos");
-            break;
-        case "osuthgra":
-            alert("osuthgra, de gastronomicos, si la trabajamos");
-            break;
-        case "ospic":
-            alert("ospic, del caucho, si la trabajamos");
-            break;
-        case "ospical":
-            alert("ospical, del calzado, si la trabajamos");
-            break;
-        default:
-            alert("Esa obra social no la conocemos, nos podes mandar mas informacion");
-            break;
+let obrasSociales = [
+    {nombre: "Obra Social de la Construccion (OSPECON)", cobertura: "Cobertura MII"},
+    {nombre: "Obra Social de los choferes y camioneros (OSCHOCA)", cobertura: "Cobertura MIS"},
+    {nombre: "Obra Social de los pasteleros (ELEVAR)", cobertura: "Cobertura MIS"},
+    {nombre: "Obra Social del personal Grafico(GRAFICOS)", cobertura: "Cobertura MII"},
+    {nombre: "Obra Social de los empleados de comercio (OSECAC)", cobertura: "Cobertura MII"},
+    {nombre: "Obra Social del personal de sanidad (OSPSA)", cobertura: "Cobertura MIS"},
+    {nombre: "Obra Social del plastico (OSPIP)", cobertura: "Cobertura MIS"},
+    {nombre: "Obra Social de las pymes y empresarios (OSDEPYM)", cobertura: "Cobertura MII"},
+    {nombre: "Obra Social del Personal de los empleados de la nacion (UP)", cobertura: "Cobertura MII"},
+    ];
+
+    function mostrarCobertura() {
+    let obraSocialSeleccionada = document.getElementById("obraSocial").value;
+    let cobertura;
+    
+    for (let i = 0; i < obrasSociales.length; i++) {
+        if (obrasSociales[i].nombre === obraSocialSeleccionada) {
+        cobertura = obrasSociales[i].cobertura;
+        break;
+        }
     }
 
-    otraObraSocial = prompt("Ingresa el nombre de tu obra social (s-salir)");
-}
-}
-
-//para la funcion vamos a usar la cantidad de sesiones que necesita realizar el niño nuevo que no esta consultando la flia
-//esta se llamaria funcion calcularSesiones
-
-let tipoDeModulo = prompt("Elegi el tipo de modulo que necesiten:\n1-Modulo Integral Simple\n2-Modulo Integral Intensivo");
-// let MIS = prompt("Este modulo cubre hasta 3 sesiones semanales");
-// let MII = prompt("Este modulo cubre hasta 5 sesiones semanales");
-
-let totalDeSesiones = parseInt(prompt("Ingrese la cantidad de sesiones que figuran en la orden que debe realizar"))
-//function calcularSesionesPorNene (){
-  //  alert("bienvenido usuario")
-//}
-
-//  const calcularSesionesPorNene = (to,psico,fono) => to + psico + fono;
-//  alert("El modulo que solicitan es: "+calcularSesionesPorNene(2,1,2));
-
-const MIS = 3;
-const MII = 5;
-
-function calcularSesionesPorNene(MIS, MII) {
-    if (MIS == totalDeSesiones) {
-        alert("Este módulo cubre hasta 3 sesiones semanales");
-    } else if (MII == totalDeSesiones) {
-    alert("Este módulo cubre hasta 5 sesiones semanales");
+    if (cobertura) {
+        alert("La obra social seleccionada cubre un Modulo Integral: " +cobertura);
     } else {
-    alert("Disculpe, no brindamos esa cantidad de sesiones");
+        alert("Disculpe, esa cantidad de sesiones no brindamos");
+    }
 }
-};
 
-calcularSesionesPorNene(MIS, MII);
-console.log(calcularSesionesPorNene);  //este console no esta chequeado que vaya a ser util
+  // Acceder a una obra social específica
+  let obraSocialSeleccionada = obrasSociales[0]; // Obra Social de la Nación Argentina (OSNA)
+  console.log(obraSocialSeleccionada.nombre); // Imprime "Obra Social de la Nación Argentina (OSNA)"
+  console.log(obraSocialSeleccionada.cobertura); // Imprime "Cobertura MII"
 
-const terapias = [
+
+
+function mostrarDisponibilidad() {
+
+    // Obtener los valores seleccionados
+
+    let especialidad = document.getElementById("especialidad").value;
+    let horario = document.getElementById("horario").value;
+
+     // Obtener la tabla de disponibilidad
+    
+    let tabla = document.getElementById("tablaDisponibilidad");
+
+     // Limpiar la tabla
+    
+    tabla.innerHTML = "";
+
+     // Mostrar la disponibilidad según la selección
+    
+    let fila = tabla.insertRow();
+    let celdaEspecialidad = fila.insertCell();
+    celdaEspecialidad.innerHTML = "Especialidad seleccionada: " + especialidad;
+    let celdaHorario = fila.insertCell();
+    celdaHorario.innerHTML = "Horario seleccionado: " + horario;
+}
+
+ // las opciones disponibles
+const elegirTerapias = document.getElementById("elegirTerapias");
+
+const especialidades =  [ 
     {
-        nombre:"Psicologia",
+        id: 1,
+        especialidad:"Psicologia",
         tipo:"cognitiva",
         cantidadMax: 2,
         disponibilidad: 40
     },
     {
-        nombre:"Psicopedagogia",
+        id: 2,
+        especialidad:"Psicopedagogia",
         tipo:"cognitiva",
         cantidadMax: 2,
         disponibilidad: 30
     },
     {
-        nombre:"Fonoaudiologia",
+        id: 3,
+        especialidad:"Fonoaudiologia",
         tipo:"cognitiva",
         cantidadMax: 1,
         disponibilidad: 20
     },
     {
-        nombre:"Terapia ocupacional",
+        id: 4,
+        especialidad:"Terapia ocupacional",
         tipo:"cognitiva/motor",
         cantidadMax: 2,
         disponibilidad: 35
     },
     {
-        nombre:"Psicomotricidad",
+        id: 5,
+        especialidad:"Psicomotricidad",
         tipo:"cognitiva/motor",
         cantidadMax: 2,
         disponibilidad: 20
     },
     {
-        nombre:"Hidroterapia",
+        id: 6,
+        especialidad:"Hidroterapia",
         tipo:"cognitiva/motor",
         cantidadMax: 1,
         disponibilidad: 30
     },
     {
-        nombre:"Kinesiologia",
+        id: 7,
+        especialidad:"Kinesiologia",
         tipo:"motor",
         cantidadMax: 2,
         disponibilidad: 25
     }
-];
+]
+;
+let especialidadPedida = []  
 
-console.table(terapias);
+especialidades.forEach((especialidades) => {
+    let content = document.createElement("div");
 
-const listaTerapiasYCantMax = terapias.map((terapias) =>{
+    content.innerHTML = `
+    <label><input type="checkbox">${especialidades.especialidad}"</label>
+    <h2>${especialidades.cantidadMax}</h2>
+    <p>${especialidades.disponibilidad}</p>
+    `;
+elegirTerapias.append(content);
+
+})
+;
+
+  // saca los tildados y guárdalos en el local 
+function guardarSeleccion() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    const seleccion = Array.from(checkboxes).map((checkbox) => checkbox.parentElement.innerText.trim());
+    localStorage.setItem('seleccionTerapias', JSON.stringify(seleccion));
+}
+  // los elementos tildados supuestamente desde el Local
+function cargarSeleccion() {
+    const seleccionGuardada = localStorage.getItem('seleccionTerapias');
+    if (seleccionGuardada) {
+    const seleccion = JSON.parse(seleccionGuardada);
+    seleccion.forEach((especialidad) => {
+    const checkbox = Array.from(document.querySelectorAll('input[type="checkbox"]')).find((checkbox) => checkbox.parentElement.innerText.trim() === especialidad);
+    if (checkbox) {
+        checkbox.checked = true;
+    }
+    });
+}
+}
+ // Agrega eventos para guardar y cargar 
+elegirTerapias.addEventListener('change', guardarSeleccion);
+window.addEventListener('load', cargarSeleccion);
+
+
+console.table(especialidades);
+
+const tipoEspecialidad = () =>{
     return{
-        nombre: terapias.nombre,
+        nombre: terapias.especialidad,
+        contidadMax: terapias.cantidadMax
+    }
+};
+
+const listaTerapiasYCantMax = especialidades.map((terapias) =>{
+    return{
+        nombre: terapias.especialidad,
         contidadMax: terapias.cantidadMax
     }
 });
 
 console.table(listaTerapiasYCantMax);
 
-terapias.sort((a,b) => b.disponibilidad - a.disponibilidad);
-console.table(terapias);
+const listaEspecialidadesYCantMax = especialidades.map((especialidad) =>{
+    return{
+        nombre: especialidad.especialidad,
+        contidadMax: especialidad.cantidadMax
+    }
+});
 
-terapias.sort((a,b) => a.nombre.localeCompare(b.nombre));
-console.table(terapias);
+console.table(listaEspecialidadesYCantMax);
 
 
-console.log(new Date(2023, 5, 12));
 
-const presupuestos2024 = new Date(2023, 9, 2, 8, 0, 0);
-const hoy = new Date(2023, 5, 12);
-console.log (presupuestos2024 - hoy);
-const milisegundosPorDia = 86400000;
 
-console.log((presupuestos2024 - hoy) / milisegundosPorDia);
+
+especialidades.sort((a,b) => b.disponibilidad - a.disponibilidad);
+console.table(especialidades);
+
+//especialidades.sort((a,b) => a.nombre.localeCompare(b.nombre));
+//console.table(especialidades);
+
+
+
+
+
+function agregarTurno() {
+    // Obtener los valores seleccionados
+    let especialidad = document.getElementById("especialidad").value;
+    let horario = document.getElementById("horario").value;
+    
+    // Obtener el cuerpo de la tabla
+    let cuerpoTabla = document.getElementById("cuerpoTabla");
+    
+    // Crear una nueva fila y agregar las celdas con los valores seleccionados
+    let fila = document.createElement("tr");
+    let celdaEspecialidad = document.createElement("td");
+    celdaEspecialidad.innerHTML = especialidad;
+    let celdaHorario = document.createElement("td");
+    celdaHorario.innerHTML = horario;
+    
+    // Agregar las celdas a la fila
+    fila.appendChild(celdaEspecialidad);
+    fila.appendChild(celdaHorario);
+    
+    // Agregar la fila al cuerpo de la tabla
+    cuerpoTabla.appendChild(fila);
+}
+
+   // Obtener el elemento select del HTML
+let selectEspecialidad = document.getElementById("especialidad");
+   // Generar las opciones del select usando el array
+for (let i = 0; i < especialidades.length; i++) {
+    let option = document.createElement("option");
+    option.value = especialidades[i];
+    option.text = especialidades[i];
+    selectEspecialidad.appendChild(option);
+  }
